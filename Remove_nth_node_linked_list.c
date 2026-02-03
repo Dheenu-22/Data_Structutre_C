3b22836e648abc9ac8974ac459a53772c93d6262
Dheenu-22 <dheenun2203@gmail.com>
2026-02-03 12:11:44 +0530

    Implement function to remove nth node from end

---
+++ Remove_nth_node_linked_list.c
@@ -0,0 +1,14 @@
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    struct ListNode dummy;
+    dummy.next = head;
+    struct ListNode *fast = &dummy, *slow = &dummy;
+    for (int i = 0; i <= n; i++) {
+        fast = fast->next;
+    }
+    while (fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    slow->next = slow->next->next;
+    return dummy.next;
+}
