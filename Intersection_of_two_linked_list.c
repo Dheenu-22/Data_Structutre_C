cecdf01fd70b48ecc983fbc3471f4b211d11d68a
Dheenu-22 <dheenun2203@gmail.com>
2026-02-09 10:06:23 +0530

    Implement function to find intersection of two linked lists

---
+++ Intersection_of_two_linked_list.c
@@ -0,0 +1,18 @@
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    if (headA == NULL || headB == NULL)
+        return NULL;
+    struct ListNode *pA = headA;
+    struct ListNode *pB = headB;
+    while (pA != pB) {
+        if (pA == NULL)
+            pA = headB;
+        else
+            pA = pA->next;
+
+        if (pB == NULL)
+            pB = headA;
+        else
+            pB = pB->next;
+    }
+    return pA; 
+}
